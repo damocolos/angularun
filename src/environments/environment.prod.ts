4ad48031d608784c4b6bfaba981470802ede2555
Damas Fajar Priyanto <damocolos@gmail.com>
2019-03-14 01:34:14 +0700

    aa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  tokenAlias: 'token-privy-id-pretest',
+    api: {
+        endpoint: 'http://pretest-qa.privydev.id'
+    }
 };
